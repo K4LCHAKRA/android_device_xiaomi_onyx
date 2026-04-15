9335b092ac90f671220b0469365179a5b83a745e
k4lchakra <k4lchakra@gmail.com>
2026-04-15 14:43:38 +0545

    onyx: move bliss libperfmgr to lineage libperfmgr

--- device.mk
+++ device.mk
@@ -389,7 +389,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/bliss/interfaces/power-libperfmgr \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
